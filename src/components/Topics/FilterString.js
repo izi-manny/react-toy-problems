7f9b061621607c8f703c5974a6a25f34f4dc4640
Emmanuel Izilov <em5750@gmail.com>
2021-11-24 21:19:42 -0500

    filter string comp updated

--- src/components/Topics/FilterString.js
+++ src/components/Topics/FilterString.js
@@ -16,14 +16,27 @@ export default class FilterString extends Component {
     this.setState({userInput: val})
   }
 
+  filterNames(userInput) {
+    let names = this.state.names
+    let filteredNames = []
+
+    for ( let i = 0; i < names.length; i++ ) {
+      if ( names[i].includes(userInput) ) {
+        filteredNames.push(names[i]);
+      }
+    }
+
+    this.setState({ filteredNames: filteredNames})
+  }
+
   render() {
     return (
       <div className="puzzleBox filterStringPB">
         <h4> Filter String </h4>
-        <span className="puzzleText"></span>
+        <span className="puzzleText">Names: { JSON.stringify(this.state.names) }</span>
         <input className="inputLine" onChange = {(e) => this.handleChange(e.target.value)} />
-        <button className="confirmationButton"> Filter </button>
-        <span className="resultsBox filterStringRB"></span>
+        <button className="confirmationButton" onClick = { () => this.filterNames(this.state.userInput) }> Filter </button>
+        <span className="resultsBox filterStringRB">Filtered Names: { JSON.stringify(this.state.filteredNames)}</span>
       </div>
     )
   }
